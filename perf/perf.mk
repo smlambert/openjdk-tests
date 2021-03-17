8cba712e1de12581b7da160ba88d9dde3db629a6
Shelley Lambert <slambert@gmail.com>
2021-03-17 15:28:23 -0400

    Try --illegal-access=warn
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- perf/perf.mk
+++ perf/perf.mk
@@ -13,4 +13,4 @@
 ##############################################################################
 
 # Create renaissance options
-RENAISSANCE_OPTIONS = --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED
+RENAISSANCE_OPTIONS_JDK16ANDUP = --illegal-access=warn
